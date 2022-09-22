92b54c3a3bfeeb555d4338b03a56bbcc8fca128f
juanosuna <jdosuna4@misena.edu.co>
2022-09-22 12:02:38 -0500

    New

--- tienda/urls.py
+++ tienda/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('contactenos/', views.contactenos, name="contactenos"),
+    path('nosotros/', views.nosotros, name="nosotros"),
 ]
